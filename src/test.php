26ebf7ec517823608af494b74b2dd9d4fd9ddb24
rlemee <rlemee01@gmail.com>
2019-12-18 17:46:46 +0100

    Mise à jour des fichiers

--- src/test.php
+++ src/test.php
@@ -1,4 +1,20 @@
 <?php
-$output = shell_exec('sudo python3 testScrap.py 123 paris');
-echo($output);
+
+$bdd = new PDO('mysql:host=localhost;dbname=Projet;charset=utf8', 'phpmyadmin', 'root');
+$reponse = $bdd->query('SELECT id, compte_twitter FROM collectivites');
+$donnees = $reponse->fetchAll();
+
+
+foreach($donnees as $donnee){
+	$id = $donnee['id'];
+	$compte_twitter = $donnee['compte_twitter'];
+	$nom = explode("/", $compte_twitter)[3];
+	echo($id . " ");
+	echo($nom . "<br/>");
+	echo('sudo python3 testScrap.py '. $id .' '. $nom);
+	
+	$output = shell_exec('sudo python3 testScrap.py '. $id .' '. $nom);
+	echo($output);
+}
+
 ?>
--- src/testScrap.py
+++ src/testScrap.py
@@ -8,6 +8,7 @@ args = parser.parse_args()
 print(args.nom)
 print(args.id)
 
+
 l = args.nom
 
 c = twint.Config()
@@ -16,9 +17,9 @@ c.Count = True
 c.User_full = False
 #c.Profile_full = True
 c.Hide_output = True
-#c.Limit = 30
+#c.Limit = 1
 c.Username = l
-c.Output = "/tmp/Output/" + args.id
+c.Output = "Output/" + str(args.id)
 c.Retweets = True
 c.Custom["user"] = ["id", "username", "join_date", "verified", "following", "followers"]
 c.Custom["tweet"] = ["id", "conversation_id", "username", "date", "link", "tweet", "photos", "video", "likes_count", "retweets_count", "replies_count", "retweet", "retweet_date"]
