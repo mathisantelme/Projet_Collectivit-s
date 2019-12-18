ae7d2bb3980f49471866c83e51ce89abf113b783
rlemee <rlemee01@gmail.com>
2019-12-18 15:58:50 +0100

    Mise à jour des fichiers

--- src/test.php
+++ src/test.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec('sudo python3 testScrap.py');
+$output = shell_exec('sudo python3 testScrap.py 123 paris');
 echo($output);
 ?>
--- src/testScrap.py
+++ src/testScrap.py
@@ -1,24 +1,31 @@
 import twint
+import argparse
 
-List = ["paris", "bordeaux", "PointeNoire116"]
+parser = argparse.ArgumentParser()
+parser.add_argument("id")
+parser.add_argument("nom")
+args = parser.parse_args()
+print(args.nom)
+print(args.id)
 
-for l in List :
-    c = twint.Config()
-    c.Store_csv = True
-    c.Count = True
-    c.User_full = False
-    c.Hide_output = True
-    #c.Profile_full = True
-    #c.Limit = 2
-    c.Username = l
-    c.Output = "/tmp/Output/" + l
-    c.Retweets = True
-    c.Custom["user"] = ["id", "username", "join_date", "verified", "following", "followers"]
-    c.Custom["tweet"] = ["id", "conversation_id", "username", "date", "link", "tweet", "photos", "video", "likes_count", "retweets_count", "replies_count", "retweet", "retweet_date"]
-    
-    
-    twint.run.Lookup(c)
-    
-    twint.run.Profile(c)
+l = args.nom
 
-    #twint.run.Search(c)
+c = twint.Config()
+c.Store_csv = True
+c.Count = True
+c.User_full = False
+#c.Profile_full = True
+c.Hide_output = True
+#c.Limit = 30
+c.Username = l
+c.Output = "/tmp/Output/" + args.id
+c.Retweets = True
+c.Custom["user"] = ["id", "username", "join_date", "verified", "following", "followers"]
+c.Custom["tweet"] = ["id", "conversation_id", "username", "date", "link", "tweet", "photos", "video", "likes_count", "retweets_count", "replies_count", "retweet", "retweet_date"]
+
+
+twint.run.Lookup(c)
+
+twint.run.Profile(c)
+
+#twint.run.Search(c)
